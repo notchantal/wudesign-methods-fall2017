da8f2ff89595f40bb4cb1382addec89ed13e8ab8
notchantal <30931736+notchantal@users.noreply.github.com>
2017-08-28 11:56:56 -0500

    Add files via upload

---
+++ color-grid/explore/script.js
@@ -0,0 +1,11 @@
+$( ".lightpink").click(function() {
+  $(this).toggleClass('lightblue');
+});
+
+$( ".mediumpink").click(function() {
+  $(this).toggleClass('mediumblue');
+});
+
+$( ".darkpink").click(function() {
+  $(this).toggleClass('darkblue');
+});
