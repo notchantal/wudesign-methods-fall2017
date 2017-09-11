4ae219272025434071ba9a6a36979b7275fc8533
notchantal <30931736+notchantal@users.noreply.github.com>
2017-09-11 00:39:50 -0500

    Add files via upload

---
+++ mouse-events/round-1/script9.js
@@ -0,0 +1,447 @@
+$('body').ready(function() {
+  $('body').css("cursor", "crosshair");
+});
+
+
+$(".box").mouseover(function() {
+  var color = randomColor();
+  $(this).css("background-color", randomColor({luminosity: 'light', hue: 'pink'}));
+});
+
+
+
+$('#b1').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "tap"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("tap");
+});
+
+
+
+$('#b2').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "button_tiny"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("button_tiny");
+});
+
+
+
+$('#b3').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "metal_plate"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("metal_plate");
+});
+
+
+
+$('#b4').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "staple_gun"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("staple_gun");
+});
+
+
+$('#b5').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "beer_can_opening"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("beer_can_opening");
+});
+
+
+$('#b6').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "bell_ring"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("bell_ring");
+});
+
+
+
+$('#b7').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "branch_break"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("branch_break");
+});
+
+
+
+
+$('#b8').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "button_click"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("button_click");
+});
+
+
+$('#b9').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "button_push"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("button_push");
+});
+
+
+
+
+$('#b10').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "camera_flashing"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("camera_flashing");
+});
+
+
+$('#b11').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "cd_tray"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("cd_tray");
+});
+
+
+
+$('#b12').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "computer_error"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("computer_error");
+});
+
+
+
+$('#b13').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "door_bell"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("door_bell");
+});
+
+
+
+$('#b14').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "door_bump"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("door_bump");
+});
+
+
+
+
+$('#b15').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "glass"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("glass");
+});
+
+
+
+$('#b16').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "keyboard_desk"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("keyboard_desk");
+});
+
+
+
+$('#b17').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "light_bulb_breaking"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("light_bulb_breaking");
+});
+
+
+$('#b18').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "pop_cork"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("pop_cork");
+});
+
+
+
+$('#b19').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "snap"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("snap");
+});
+
+
+
+$('#b20').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "water_droplet"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("water_droplet");
+});
+
+$('#b21').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "glass"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("glass");
+});
+
+$('#b22').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "glass"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("glass");
+});
+
+$('#b23').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "glass"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("glass");
+});
+
+$('#b24').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "glass"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("glass");
+});
+
+$('#b25').mouseover(function() {
+ion.sound({
+    sounds: [
+        {name: "glass"},
+    ],
+
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
+// play sound
+ion.sound.play("glass");
+});
