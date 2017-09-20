9144e06fa2cce97edba997a4a1f2336b357b6d16
notchantal <30931736+notchantal@users.noreply.github.com>
2017-09-20 09:26:27 -0500

    Add files via upload

--- mouse-events/round-3/script2.js
+++ mouse-events/round-3/script2.js
@@ -1,7 +1,9 @@
 $(".container").mouseover(function() {
 	var word = ["hello", "bonjour", "hola", "guten tag", "ciao", "namaste", "salaam", "marhaba", "szia", "ni hau", "sain bainuu"]
-	var selectedword = Math.floor(Math.random()*word.length);
-	$("h1").html(""+word[selectedword]+"");
+	$("h1").each(function(){
+		var selectedword = Math.floor(Math.random()*word.length);
+		$(this).html(""+word[selectedword]+"");
+		});
 	$("h1").toggleClass("mono rotate lettersp")
 });
 
