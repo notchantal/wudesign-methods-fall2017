3803635d6e3656984d778e44c2948fc1f46f9f81
notchantal <30931736+notchantal@users.noreply.github.com>
2017-09-25 11:19:39 -0500

    Add files via upload

--- mouse-events/round-4/script9.js
+++ mouse-events/round-4/script9.js
@@ -1,14 +1,31 @@
 $("h1").mouseover(function(){
 	$(this).css({
-		"font-size": '200px',
+		"font-size": '180px',
 		"position": 'absolute',
-		"opacity": '0.05'
+		"opacity": '0.1'
 	});
 		var word = ["hello", "مرحبا","你好", "හෙලෝ", "kamusta", "zdravo", "ہیلو", "नमस्ते", "xin chào", "добры дзень", "bonjour", "hola", "guten tag", "ciao", "namaste", "salaam", "halò", "שלום", "sveiki", "สวัสดี", "bonghjornu", "szia", "ສະບາຍດີ", "ni hau", "Здравейте", "sain bainuu"]
 	$(this).each(function(){
 		var selectedword = Math.floor(Math.random()*word.length);
 		$(this).html(""+word[selectedword]+"");
 		});
+});
+
+$("h2").mouseover(function(){
+	$(this).css({
+		"font-size": '17px',
+		"font-weight": 'bold'
+	});
+		var word = ["hello", "مرحبا","你好", "හෙලෝ", "kamusta", "zdravo", "ہیلو", "नमस्ते", "xin chào", "добры дзень", "bonjour", "hola", "guten tag", "ciao", "namaste", "salaam", "halò", "שלום", "sveiki", "สวัสดี", "bonghjornu", "szia", "ສະບາຍດີ", "ni hau", "Здравейте", "sain bainuu"]
+	$(this).each(function(){
+		var selectedword = Math.floor(Math.random()*word.length);
+		$(this).html(""+word[selectedword]+"");
+		});
+});
+
+$('.line').mouseover(function() {
+  $(this).toggleClass("skew big");
+  $(".line").css("background-color", randomColor({luminosity: "dark", hue: "red"}));
 });
 
 
