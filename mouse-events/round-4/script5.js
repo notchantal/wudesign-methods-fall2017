f9a7d0d37d65b52bd12d7a4146f909f7ad6358da
notchantal <30931736+notchantal@users.noreply.github.com>
2017-09-25 09:49:17 -0500

    Add files via upload

---
+++ mouse-events/round-4/script5.js
@@ -0,0 +1,16 @@
+$("h1").click(function(){
+	$(this).css({"color": randomColor({luminosity: 'light', hue: 'red'})});
+		var word = ["hello", "مرحبا","你好", "හෙලෝ", "kamusta", "zdravo", "ہیلو", "नमस्ते", "xin chào", "добры дзень", "bonjour", "hola", "guten tag", "ciao", "namaste", "salaam", "halò", "שלום", "sveiki", "สวัสดี", "bonghjornu", "szia", "ສະບາຍດີ", "ni hau", "Здравейте", "sain bainuu"]
+	$(this).each(function(){
+		var selectedword = Math.floor(Math.random()*word.length);
+		$(this).html(""+word[selectedword]+"");
+		});
+});
+
+$("h1").hover(function(){
+	$(this).toggleClass("big");
+});
+
+// $("h1").hover(function(){
+// 	$(this).css("font-size", '10px');
+// });
